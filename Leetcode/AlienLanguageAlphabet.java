e8a50ce4ba99e04f2e43688f41e57ca7f7b4c95c
ishansrivastava90 <ishan@seas.upenn.edu>
2021-01-24 21:03:20 -0800

    figuring out alien language!

---
+++ Leetcode/AlienLanguageAlphabet.java
@@ -0,0 +1,10 @@
+package Leetcode;
+
+import java.lang.System;
+
+public class AlienLanguageAlphabet {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        return;    
+    }
+}
